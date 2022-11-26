73ac6b20a519dbb737ca6d914fadb0c65ec9e76b
temptado <jason1287712@gmail.com>
2022-11-25 20:28:48 -0500

    added script to change password to text

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+const showHiddenPass = (inputPass, inputIcon) => {
+    const input = document.getElementById(inputPass),
+        iconEye = document.getElementById(inputIcon);
+
+    iconEye.addEventListener('click', () => {
+        if (input.type === 'password') {
+            input.type = 'text';
+
+            iconEye.classList.add('ri-eye-line');
+
+            iconEye.classList.remove('ri-eye-off-line')
+        } else {
+            input.type = 'password';
+
+            iconEye.classList.add('ri-eye-off-line');
+
+            iconEye.classList.remove('ri-eye-line');
+        }
+    });
+}
+
+showHiddenPass('input-pass', 'input-icon');
